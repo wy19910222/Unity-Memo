ed5ba55bd2b19edbaf20ae16ffbaa527d9984625
yun.wang <wy1991022219910222@163.com>
2024-07-01 00:24:56 +0800

    Memo6_GetFieldPath

---
+++ Assets/Memo6_GetFieldPath/Editor/FieldPathUtils.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-07-01 00:16:28 640
+ * @LastEditor: wangyun
+ * @EditTime: 2024-07-01 00:16:28 644
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+public static class FieldPathUtils {
+	public static string GetFieldPath<TType, TValue>(Expression<Func<TType, TValue>> expr) {
+		List<string> member = new List<string>();
+		if (expr.Body.NodeType == ExpressionType.MemberAccess) {
+			MemberExpression me = expr.Body as MemberExpression;
+			while (me != null) {
+				member.Add(me.Member.Name);
+				me = me.Expression as MemberExpression;
+			}
+			member.Reverse();
+		}
+		return string.Join(".", member);
+	}
+}
---
+++ Assets/Memo6_GetFieldPath/Editor/GetFieldPathDemo.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-07-01 00:21:17 715
+ * @LastEditor: wangyun
+ * @EditTime: 2024-07-01 00:21:17 719
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+public static class GetFieldPathDemo {
+	[MenuItem("Memo/Memo6/GetFieldPathDemo")]
+	public static void Test() {
+		Debug.Log( FieldPathUtils.GetFieldPath((GameObject go) => go.transform.position.magnitude));
+	}
+}
