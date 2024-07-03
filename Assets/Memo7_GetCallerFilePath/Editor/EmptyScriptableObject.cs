d2b6953f5ba5ce5c6947b33813a8ef82f32fce46
yun.wang <wy1991022219910222@163.com>
2024-07-03 20:28:01 +0800

    Memo7_GetCallerFilePath

---
+++ Assets/Memo7_GetCallerFilePath/Editor/EmptyScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-07-03 19:58:05 935
+ * @LastEditor: wangyun
+ * @EditTime: 2024-07-03 19:58:05 938
+ */
+
+using UnityEngine;
+
+public class EmptyScriptableObject : ScriptableObject {
+}
