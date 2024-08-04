a6dd6703379ef477cccda6a257feb179228ce722
yun.wang <wy1991022219910222@163.com>
2024-08-05 00:37:31 +0800

    Memo11_SharedMemory

---
+++ Assets/Memo11_SharedMemory/Editor/SharedMemory.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-08-04 22:00:45 615
+ * @LastEditor: wangyun
+ * @EditTime: 2024-08-04 22:00:45 619
+ */
+
+using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using UnityEngine;
+
+namespace Memo11_SharedMemory.Editor {
+	public class SharedMemory : IDisposable {
+		public int Capacity { get; }
+		public string MemoryMappedFileName { get; }
+		public string MutexName { get; }
+		
+		private readonly MemoryMappedFile m_File;
+		private readonly Mutex m_Mutex;
+
+		public SharedMemory(string name, int capacity = 1024) {
+			if (capacity <= 0) {
+				throw new ArgumentException("无效容量！", nameof(capacity));
+			}
+			if (capacity > int.MaxValue - 4) {
+				throw new ArgumentException($"容量溢出，请将容量设置为{int.MaxValue - 4}以内！", nameof(capacity));
+			}
+			Capacity = capacity;
+			MemoryMappedFileName = "MemoryMappedFile_" + name;
+			MutexName = "Mutex_" + name;
+			m_File = MemoryMappedFile.CreateOrOpen(MemoryMappedFileName, Capacity + 4);
+			// name不能与MemoryMappedFile的name相同，否则拿不到锁
+			m_Mutex = new Mutex(false, MutexName);
+		}
+
+		public void SetString(string data) {
+			SetData(data == null ? null : System.Text.Encoding.UTF8.GetBytes(data));
+		}
+
+		public void SetData(byte[] data) {
+			int dataLength = data == null ? 0 : data.Length;
+			if (dataLength > Capacity) {
+				throw new ArgumentException("数据长度超出容量！", nameof(data));
+			}
+			if (m_Mutex.WaitOne()) {
+				using (MemoryMappedViewAccessor writer = m_File.CreateViewAccessor(0, dataLength + 4)) {
+					byte[] bytes = BitConverter.GetBytes(dataLength);
+					writer.WriteArray(0, bytes, 0, 4);
+					if (dataLength > 0) {
+						writer.WriteArray(4, data, 0, dataLength);
+					}
+				}
+				m_Mutex.ReleaseMutex();
+			} else {
+				Debug.LogError("获取锁失败！");
+			}
+		}
+
+		public string GetString() {
+			byte[] data = GetData();
+			return data == null ? null : System.Text.Encoding.UTF8.GetString(data);
+		}
+
+		public byte[] GetData() {
+			if (m_Mutex.WaitOne()) {
+				int dataLength;
+				using (MemoryMappedViewAccessor reader = m_File.CreateViewAccessor(0, 4)) {
+					byte[] bytes = new byte[4];
+					reader.ReadArray(0, bytes, 0, 4);
+					dataLength = BitConverter.ToInt32(bytes, 0);
+				}
+				byte[] data;
+				if (dataLength > 0) {
+					data = new byte[dataLength];
+					using (MemoryMappedViewAccessor reader = m_File.CreateViewAccessor(4, dataLength)) {
+						reader.ReadArray(0, data, 0, dataLength);
+					}
+				} else {
+					data = Array.Empty<byte>();
+				}
+				m_Mutex.ReleaseMutex();
+				return data;
+			} else {
+				Debug.LogError("获取锁失败！");
+				return null;
+			}
+		}
+
+		public void Dispose() {
+			m_File.Dispose();
+			m_Mutex.Dispose();
+		}
+	}
+}
---
+++ Assets/Memo11_SharedMemory/Editor/SharedMemorySample.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Memo11_SharedMemory.Editor {
+	public class SharedMemorySampleWindow : EditorWindow {
+		[MenuItem("Memo/Memo011/SharedMemorySampleWindow", priority = 11)]
+		public static void ShowWindow() {
+			var window = GetWindow<SharedMemorySampleWindow>();
+			window.minSize = new Vector2(300F, 160F);
+			window.Show();
+		}
+
+		private string m_Words;
+		private SharedMemory m_SharedMemory;
+
+		private void OnEnable() {
+			m_SharedMemory = new SharedMemory("SharedMemorySample", 128);
+		}
+
+		private void OnDisable() {
+			m_SharedMemory.Dispose();
+		}
+
+		private void OnGUI() {
+			EditorGUILayout.HelpBox("请打开多个工程的本窗口查看效果。", MessageType.Info);
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("当前共享值", GUILayout.Width(EditorGUIUtility.labelWidth - 1F));
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.TextField(m_SharedMemory.GetString());
+			EditorGUI.EndDisabledGroup();
+			EditorGUILayout.EndHorizontal();
+			
+			GUILayout.Space(10F);
+			
+			string newWords = EditorGUILayout.TextField("准备设置的值", m_Words);
+			if (newWords != m_Words) {
+				Undo.RecordObject(this, "Words");
+				m_Words = newWords;
+			}
+			if (GUILayout.Button("设置", GUILayout.Height(EditorGUIUtility.singleLineHeight * 2F))) {
+				m_SharedMemory.SetString(m_Words);
+			}
+		}
+
+		private void Update() {
+			Repaint();
+		}
+	}
+}
